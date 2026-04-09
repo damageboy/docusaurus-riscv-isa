9a199b8e2019e20a9eb6f7589612125363e04b25
Dan Shechter <dans@houmus.org>
2026-04-09 23:47:05 +0200

    feat: Docusaurus scaffold for RISC-V ISA manual

---
+++ docusaurus.config.js
@@ -0,0 +1,77 @@
+// @ts-check
+import { themes as prismThemes } from 'prism-react-renderer';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'RISC-V ISA Manual',
+  tagline: 'The RISC-V Instruction Set Architecture Manual',
+  favicon: 'img/favicon.ico',
+
+  url: 'https://riscv.org',
+  baseUrl: '/',
+
+  onBrokenLinks: 'warn',
+  onBrokenMarkdownLinks: 'warn',
+
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: './sidebars.js',
+          remarkPlugins: [remarkMath],
+          rehypePlugins: [rehypeKatex],
+        },
+        blog: false,
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      }),
+    ],
+  ],
+
+  stylesheets: [
+    {
+      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
+      type: 'text/css',
+      integrity: 'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
+      crossorigin: 'anonymous',
+    },
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'RISC-V ISA Manual',
+        items: [
+          {
+            type: 'docSidebar',
+            sidebarId: 'unprivilegedSidebar',
+            position: 'left',
+            label: 'Unprivileged',
+          },
+          {
+            type: 'docSidebar',
+            sidebarId: 'privilegedSidebar',
+            position: 'left',
+            label: 'Privileged',
+          },
+        ],
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+    }),
+};
+
+export default config;
